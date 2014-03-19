ae94673db08da0e6f6b4acd89ec7eb1a56c20764
dderamou <dderamou@DDERAMOU-BE.be.oracle.com>
2014-03-19 17:10:26 +0100

    javadoc to all methods

--- com/deramoudt/velleman/edu05/Edu05Library.java
+++ com/deramoudt/velleman/edu05/Edu05Library.java
@@ -22,34 +22,83 @@ import com.sun.jna.Native;
 
 
 public interface Edu05Library extends Library {
-        
-        Edu05Library INSTANCE = (Edu05Library)
-            Native.loadLibrary("EDU05D",Edu05Library.class);
-        
-        int OpenDevice() ;
-        void CloseDevice() ;
-        int Connected() ;
-            
-        void InOutMode(int IOMode) ;
-        
-        //Analog to Digital converter function
-        int ReadAnalogChannel(int Channel) ;
-        
-        //PWM Output function
-        void SetPWM(int Channel, int Data) ;//Sets the status the PWM output
-        
-        //Digital Output functions
-        void OutputAllDigital(int Data) ;//Sets the digital outputs according to the data
-        void ClearDigitalChannel(int Channel) ;//Clears the output channel
-        void ClearAllDigital() ;//Clears all output channels
-        void SetDigitalChannel(int Channel) ;//Sets the output channel
-        void SetAllDigital(); //Sets all output channels
-       
-        //Digital Input functions
-        int ReadAllDigital() ; // Reads the status of all the input channels
-       
-        //LCD functions
-        void LCDInit(); //Initializes the LCD display module
-        void LCDClear(); //Clears the text on the LCD display
-        void  LCDWriteString(String Data, int Position); // 
-    }
+
+    Edu05Library INSTANCE = (Edu05Library) Native.loadLibrary("EDU05D", Edu05Library.class);
+
+    /**
+     *
+     * @return int status of opened device
+     */
+    int OpenDevice();
+
+    /**
+     * closes device connectivity from the PC.
+     */
+    void CloseDevice();
+
+    /**
+     * @return int status of connectivity
+     */
+    int Connected();
+
+
+    /**
+     * @param IOMode
+     */
+    void InOutMode(int IOMode);
+
+    //Analog to Digital converter function
+
+    /**
+     * @param Channel 1-5
+     * @return
+     */
+    int ReadAnalogChannel(int Channel);
+
+    //PWM Output function
+
+    /**
+     * @param Channel
+     * @param Data
+     */
+    void SetPWM(int Channel, int Data); //Sets the status the PWM output
+
+    //Digital Output functions
+
+    /**
+     * @param Data
+     */
+    void OutputAllDigital(int Data); //Sets the digital outputs according to the data
+
+    /**
+     * @param Channel 1 to 8
+     */
+    void ClearDigitalChannel(int Channel); //Clears the output channel
+
+    void ClearAllDigital(); //Clears all output channels
+
+    /**
+     * @param Channel 1 to 8
+     */
+    void SetDigitalChannel(int Channel); //Sets the output channel
+
+    void SetAllDigital(); //Sets all output channels
+
+    //Digital Input functions
+
+    /**
+     * @return
+     */
+    int ReadAllDigital(); // Reads the status of all the input channels
+
+    //LCD functions
+    void LCDInit(); //Initializes the LCD display module
+
+    void LCDClear(); //Clears the text on the LCD display
+
+    /**
+     * @param Data String to be displayed on the LCD 
+     * @param Position where to set first character, starting from the left.
+     */
+    void LCDWriteString(String Data, int Position); //
+}
